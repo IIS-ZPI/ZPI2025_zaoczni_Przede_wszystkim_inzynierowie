a86752c11cb26935eb9c5a0b15b60a824d790263
Jakub Lewandowski <lewando54@gmail.com>
2025-12-14 11:05:46 +0100

    Release v1.1.0 (#17)
    * Wypisywanie nazwy grupy i scrum mastera
    
    * Feature lab1 zad3/print id julie m99 (#3)
    
    * Wypisywanie nazwy grupy i scrum mastera
    
    * Print GitHub id JulieM99
    
    ---------
    
    Co-authored-by: LeskiewiczMichal <98560352+LeskiewiczMichal@users.noreply.github.com>
    
    * Add my GithubID to main (#5)
    
    * Feature lab1 zad3/print id 246331 (#6)
    
    * Wypisywanie nazwy grupy i scrum mastera
    
    * print gitjub ID
    
    ---------
    
    Co-authored-by: LeskiewiczMichal <98560352+LeskiewiczMichal@users.noreply.github.com>
    
    * Addition implementation (zad 4)
    
    * New feature (zad 6)
    
    * Revert "New feature (zad 6)" 
    
    This reverts commit 7603f0eafe0c554da89c160dc84e840749c1c6a1.
    
    * implement difference (#11)
    
    * Implementation of Multiplication method
    
    * Adding comment for exercise 6
    
    * Revert "Adding comment for exercise 6" (#14)
    
    This reverts commit 2eca9b98bc7ea663b27d5b572f87adc7319f2558.
    
    * Implement Division (#15)
    
    ---------
    
    Co-authored-by: LeskiewiczMichal <leskiewicz02@gmail.com>
    Co-authored-by: LeskiewiczMichal <98560352+LeskiewiczMichal@users.noreply.github.com>
    Co-authored-by: JulieM99 <109237951+JulieM99@users.noreply.github.com>
    Co-authored-by: 246331 <114869207+246331@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 print("Grupa: Przede wszystkim inżynierowie\nDevOps Scrum Master LeskiewiczMichal")
+print("246331")
+print("JulieM99")
+print("lewando54")
+
+
+def Addition(a, b):
+    return a + b
+
+def Difference(a,b):
+    return a - b
+
+def Multiplication(a, b):
+    return a * b
+
+def Division(a, b):
+    return a / b
